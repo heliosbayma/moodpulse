99f27ca96ad39d68335ad9f117a4f1847f5d02ba
Helios Bayma <heliosbayma@gmail.com>
2023-09-01 17:58:13 -0400

    Adds logic to new-user page and adds Loader component

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,7 +1,35 @@
+import Loader from '@/components/Loader'
+import prisma from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+async function createNewUser() {
+  const user = await currentUser()
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
 const NewUser = () => {
+  createNewUser()
+
   return (
-    <section>
-      <h1>New User</h1>
+    <section className="flex h-screen w-screen items-center justify-center ">
+      <Loader text="Setting up your journal..." />
     </section>
   )
 }
