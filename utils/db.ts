4d6a223b09e2436e5b0030c9381f99ab24e00d37
Helios Bayma <heliosbayma@gmail.com>
2023-09-01 15:59:39 -0400

    Adds type and other small changes to PrismaClient

--- utils/db.ts
+++ utils/db.ts
@@ -1,13 +1,22 @@
+//  this avoids multiple instances of Prisma Client in development
+//  see: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log: ['query'],
   })
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
