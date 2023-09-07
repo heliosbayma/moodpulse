74c5015922f799936a6218f13de5555abed52e05
Helios Bayma <heliosbayma@gmail.com>
2023-09-07 13:49:05 -0400

    Get user journal entries from db

--- src/app/(dashboard)/journal/page.tsx
+++ src/app/(dashboard)/journal/page.tsx
@@ -1,4 +1,23 @@
-const JournalPage = () => {
+import prisma from '@/utils/db'
+import getUserByClerkId from '@/utils/auth'
+
+const getEntries = async () => {
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
   return (
     <>
       <section>moodboard</section>
