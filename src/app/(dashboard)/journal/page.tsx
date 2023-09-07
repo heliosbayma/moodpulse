bd9a09f01dd9e94e3d680259f09cc2bacf6e460d
Helios Bayma <heliosbayma@gmail.com>
2023-09-07 16:02:17 -0400

    Adds moodboard and small tweaks

--- src/app/(dashboard)/journal/page.tsx
+++ src/app/(dashboard)/journal/page.tsx
@@ -3,7 +3,19 @@ import getUserByClerkId from '@/utils/auth'
 
 const getEntries = async () => {
   const user = await getUserByClerkId()
-  const entries = await prisma.journalEntry.findMany({
+  const today = new Date()
+  const thirtyDaysAgo = new Date(today.setDate(today.getDate() - 30))
+
+  const moodEntries = await prisma.moodEntry.findMany({
+    where: {
+      userId: user.id,
+      createdAt: {
+        gte: thirtyDaysAgo,
+      },
+    },
+  })
+
+  const journalEntries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
     },
@@ -12,12 +24,13 @@ const getEntries = async () => {
     },
   })
 
-  return entries
+  return { journalEntries, moodEntries }
 }
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  console.log('entries', entries)
+  console.log('moodboard', entries.moodEntries)
+  console.log('entries', entries.journalEntries)
   return (
     <>
       <section>moodboard</section>
