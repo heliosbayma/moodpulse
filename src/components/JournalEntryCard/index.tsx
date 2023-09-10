f6668bf52db16a8b828ff6b63449a3565f116abb
Helios Bayma <heliosbayma@gmail.com>
2023-09-10 16:49:45 -0400

    Adds card layout to journal entries

--- src/components/JournalEntryCard/index.tsx
+++ src/components/JournalEntryCard/index.tsx
@@ -9,11 +9,15 @@ const JournalEntryCard: FC<JournalEntryCardProps> = ({ journalEntry }) => {
   const date = new Date(journalEntry.createdAt).toDateString()
 
   return (
-    <>
-      <h1>{journalEntry.title}</h1>
-      <p>{journalEntry.content}</p>
-      <p className="px-4 py-5 sm:px-6">{date}</p>
-    </>
+    <article className="bg-white w-100 flex flex-col overflow-hidden rounded-lg px-6 py-4 shadow-lg">
+      <time className="text-gray-300 py-1 text-xs font-semibold uppercase tracking-wide">
+        {date}
+      </time>
+      <h3 className="truncate text-lg font-semibold leading-tight">
+        {journalEntry.title}
+      </h3>
+      <p className="mt-1">{journalEntry.content}</p>
+    </article>
   )
 }
 
