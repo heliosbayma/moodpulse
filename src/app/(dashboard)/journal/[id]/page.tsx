03cd2fc52cb4e1e72ba6188297ce1bcfec0a6369
Helios Bayma <heliosbayma@gmail.com>
2023-09-12 09:30:22 -0400

    [journal] gets data for journal page

--- src/app/(dashboard)/journal/[id]/page.tsx
+++ src/app/(dashboard)/journal/[id]/page.tsx
@@ -1,18 +1,40 @@
+import Editor from '@/components/Editor'
+import getUserByClerkId from '@/utils/auth'
+import prisma from '@/utils/db'
+import { JournalEntry } from '@prisma/client'
 import { FC } from 'react'
 
 type Params = {
-  id: string
-  content: string
+  params: {
+    id: string
+  }
 }
 
-const JournalEntryPage: FC<Params> = ({ id, content }) => {
+const JournalEntryPage: FC<Params> = async ({ params }) => {
+  const entry = await getEntry(params.id)
+
   return (
     <div>
       <h1>JournalEntryPage</h1>
-      <p>{id}</p>
-      <p>{content}</p>
+      <Editor entry={entry} />
     </div>
   )
 }
 
 export default JournalEntryPage
+
+const getEntry = async (id: string) => {
+  const user = await getUserByClerkId()
+  if (!user) throw new Error('User not found')
+
+  const entry: JournalEntry | null = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
