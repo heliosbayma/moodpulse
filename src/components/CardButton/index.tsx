d2713e9f846b4b251fbc96e002706d0118f368bc
Helios Bayma <heliosbayma@gmail.com>
2023-09-12 09:32:55 -0400

    [journal] Creates CardButton as client component, keeping Card as server.

---
+++ src/components/CardButton/index.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
+import { useRouter } from 'next/navigation'
+import { FC } from 'react'
+
+interface CardButtonProps {
+  label: string
+  id: string
+  className?: string
+}
+
+const CardButton: FC<CardButtonProps> = ({ label, id, className = '' }) => {
+  const router = useRouter()
+
+  return (
+    <button
+      className={className}
+      onClick={() => handleButtonClick(label, id, router)}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default CardButton
+
+const handleButtonClick = (
+  label: string,
+  id: string,
+  router: AppRouterInstance,
+) => {
+  if (label === 'Open') {
+    handleOpen(id, router)
+  } else if (label === 'Delete') {
+    handleDelete(id)
+  }
+}
+
+const handleOpen = (id: string, router: AppRouterInstance) => {
+  router.push(`/journal/${id}`)
+}
+
+const handleDelete = (id: string) => {
+  console.log('delete', id)
+}
