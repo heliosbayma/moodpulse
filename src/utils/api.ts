16c1b607ae665861e38a24e111bf611a7ec3cadd
Helios Bayma <heliosbayma@gmail.com>
2023-09-13 10:44:07 -0400

    [api] improve error handling

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,16 +3,41 @@ const createURL = (path: string) => {
 }
 
 export const createNewEntry = async () => {
-  const res = await fetch(
-    new Request(createURL('/api/entry'), {
-      method: 'POST',
-      body: JSON.stringify({ content: 'new entry' }),
-    }),
-  )
+  try {
+    const res = await fetch(
+      new Request(createURL('/api/entry'), {
+        method: 'POST',
+        body: JSON.stringify({ content: 'new entry' }),
+      }),
+    )
 
-  if (res.ok) {
-    return res.json()
-  } else {
-    throw new Error('Something went wrong on API server!')
+    if (res.ok) {
+      return { error: false, code: res.status, message: res.json() }
+    } else {
+      return { error: true, code: res.status, message: res.statusText }
+    }
+  } catch (error) {
+    console.error(error)
+    return { error: true, code: 500, message: 'Internal Server Error' }
+  }
+}
+
+export const updateEntry = async (id: string, content: string) => {
+  try {
+    const res = await fetch(
+      new Request(createURL(`/api/entry/${id}`), {
+        method: 'PATCH',
+        body: JSON.stringify({ content }),
+      }),
+    )
+
+    if (res.ok) {
+      return { error: false, code: res.status, message: res.json() }
+    } else {
+      return { error: true, code: res.status, message: res.statusText }
+    }
+  } catch (error) {
+    console.error(error)
+    return { error: true, code: 500, message: 'Internal Server Error' }
   }
 }
