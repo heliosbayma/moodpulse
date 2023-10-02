0cab2990ad43298e1a5703eb63ac08dcb1a6179c
Helios Bayma <heliosbayma@gmail.com>
2023-10-02 16:09:26 -0400

    [journal] only show moodboard title when they exist in db

--- src/app/(dashboard)/journal/page.tsx
+++ src/app/(dashboard)/journal/page.tsx
@@ -1,9 +1,39 @@
-import getUserByClerkId from '@/utils/auth'
+import { getUserByClerkId } from '@/utils/auth'
 import JournalEntryCard from '@/components/JournalEntryCard'
 import prisma from '@/utils/db'
 import { JournalEntry, MoodEntry, User } from '@prisma/client'
 import NewJournalEntryButton from '@/components/NewJournalEntryButton'
 import { redirect } from 'next/navigation'
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  const journalEntries: JournalEntry[] = entries.journalEntries
+  const moodEntries: MoodEntry[] = entries.moodEntries
+
+  return (
+    <>
+      {moodEntries && moodEntries.length > 0 && (
+        <>
+          <h2 className="mb-2 mr-8 text-3xl">Moodboard</h2>
+          <section className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-3 2xl:grid-cols-4">
+            {moodEntries.map((entry) => (
+              <div key={entry.id} />
+            ))}
+          </section>
+        </>
+      )}
+      <h2 className="mb-2 mr-8 text-3xl">Journal</h2>
+      <NewJournalEntryButton />
+      <section className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-3 2xl:grid-cols-4">
+        {journalEntries.map((entry) => (
+          <JournalEntryCard key={entry.id} journalEntry={entry} />
+        ))}
+      </section>
+    </>
+  )
+}
+
+export default JournalPage
 
 const getEntries = async () => {
   const user: User | null | undefined = await getUserByClerkId()
@@ -33,24 +63,3 @@ const getEntries = async () => {
 
   return { journalEntries, moodEntries }
 }
-
-const JournalPage = async () => {
-  const entries = await getEntries()
-  const journalEntries: JournalEntry[] = entries.journalEntries
-  const moodEntries: MoodEntry[] = entries.moodEntries
-
-  return (
-    <>
-      <h2 className="mb-2 mr-8 text-3xl">Moodboard</h2>
-      <h2 className="mb-2 mr-8 text-3xl">Journal</h2>
-      <NewJournalEntryButton />
-      <section className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-3 2xl:grid-cols-4">
-        {journalEntries.map((entry) => (
-          <JournalEntryCard key={entry.id} journalEntry={entry} />
-        ))}
-      </section>
-    </>
-  )
-}
-
-export default JournalPage
